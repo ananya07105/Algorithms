67a37827751d63689087951033c33c058149226f
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2021-09-30 21:43:52 +0530

    Create O(n)time.cpp

---
+++ Stack/MinStack/O(n)time.cpp
@@ -0,0 +1,30 @@
+stack<int> s;
+stack<int> mn;
+MinStack::MinStack() {
+    while(!s.empty()) s.pop();
+    while(!mn.empty()) mn.pop();
+}
+
+void MinStack::push(int x) {
+    s.push(x);
+    if(mn.empty() or x<mn.top()) 
+      mn.push(x);
+}
+
+void MinStack::pop() {
+    if(s.empty()) return;
+    if(mn.top()=s.top()) mn.pop();
+    s.pop();
+ 
+}
+
+int MinStack::top() {
+    if(s.empty()) return -1;
+    return s.top();
+}
+
+int MinStack::getMin() {
+    if(mn.empty()) return -1;
+    return mn.top();
+}
+
